0463695232d146699ca0a6c8be1bbd423f0edc19
shinichi-46 <poronupu.mazeran@gmail.com>
2023-02-11 14:43:50 +0900

    スクロールできるようにした

--- lib/view/pages/account/again_edit_account_page.dart
+++ lib/view/pages/account/again_edit_account_page.dart
@@ -63,80 +63,82 @@ class _AgainEditAccountPageState extends ConsumerState<AgainEditAccountPage> {
               },
             ),
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                     GestureDetector(
-                       onTap: () async {
-                         await selectImage();
-                       },
-                       child: image == null
-                           ? ref.watch(accountNotifierProvider).iconImagePath == '' ? Container(
-                              height: 100,
-                              width: 100,
-                              decoration: BoxDecoration(
-                                border: Border.all(color: Colors.black),
-                                shape: BoxShape.circle,
-                              ),
-                              child: Icon(
-                                Icons.add_a_photo,
-                                size: 50),
-                            ) : CircleAvatar(
-                             radius: 50,
-                             backgroundImage: NetworkImage(ref.watch(accountNotifierProvider).iconImagePath!))
-                          : CircleAvatar(
-                            radius: 50,
-                            backgroundImage: FileImage(image!))
-                       ),
-                  Container(
-                    height: 70,
-                    child: Center(
-                      child: Text(
-                        'ユーザー名',
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 100,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 50.0,right: 50.0),
-                      child: TextField(
-                        controller: _userNameController,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 50,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'お気に入り施設',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    ),
-                  ),
-                  for (int index = 1 ; index<ref.watch(accountNotifierProvider).favoritePlaceList!.length+1; index++) Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          child: Center(child: Text('${index.toString()}')),
-                          height: 30,
-                          width: 50,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black),
-                            shape: BoxShape.circle,
-                          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                       GestureDetector(
+                         onTap: () async {
+                           await selectImage();
+                         },
+                         child: image == null
+                             ? ref.watch(accountNotifierProvider).iconImagePath == '' ? Container(
+                                height: 100,
+                                width: 100,
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Icon(
+                                  Icons.add_a_photo,
+                                  size: 50),
+                              ) : CircleAvatar(
+                               radius: 50,
+                               backgroundImage: NetworkImage(ref.watch(accountNotifierProvider).iconImagePath!))
+                            : CircleAvatar(
+                              radius: 50,
+                              backgroundImage: FileImage(image!))
+                         ),
+                    Container(
+                      height: 70,
+                      child: Center(
+                        child: Text(
+                          'ユーザー名',
+                          style: TextStyle(fontSize: 30),
                         ),
                       ),
-                      Text('${ref.watch(accountNotifierProvider).favoritePlaceList![index-1]}'),
-                    ],
-                  )
-                ],
-              ),
+                    ),
+                    SizedBox(
+                      height: 100,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 50.0,right: 50.0),
+                        child: TextField(
+                          controller: _userNameController,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'お気に入り施設',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    for (int index = 1 ; index<ref.watch(accountNotifierProvider).favoritePlaceList!.length+1; index++) Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            child: Center(child: Text('${index.toString()}')),
+                            height: 30,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black),
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                        ),
+                        Text('${ref.watch(accountNotifierProvider).favoritePlaceList![index-1]}'),
+                      ],
+                    )
+                  ],
+                ),
+            ),
           ),
       ),
     );
--- lib/view/pages/post/create_post_page.dart
+++ lib/view/pages/post/create_post_page.dart
@@ -20,244 +20,246 @@ class CreatePostPage extends StatelessWidget {
       child: Scaffold(
           appBar: AppBar(
           ),
-          body: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 80,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 10,top: 10),
-                    child: Text(
-                      '記録',
-                      style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold),
+          body: SingleChildScrollView(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 80,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10,top: 10),
+                      child: Text(
+                        '記録',
+                        style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold),
+                      ),
                     ),
                   ),
-                ),
-                Divider(
+                  Divider(
+                      height: 1,
+                      thickness: 0.2,
+                      indent: 10,
+                      endIndent: 10,
+                      color: Colors.black,
+                    ),
+                  Container(
+                    height: 50,
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10,),
+                          child: Text(
+                            '日時',
+                            style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10,),
+                          child: Text(
+                            'yyyy年/M月/D日',
+                            style: TextStyle(fontSize: 15,)
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
                     height: 1,
                     thickness: 0.2,
                     indent: 10,
                     endIndent: 10,
                     color: Colors.black,
                   ),
-                Container(
-                  height: 50,
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10,),
-                        child: Text(
-                          '日時',
-                          style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      Spacer(),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10,),
-                        child: Text(
-                          'yyyy年/M月/D日',
-                          style: TextStyle(fontSize: 15,)
-                        ),
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text(
+                        '評価',
+                      style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-                Divider(
-                  height: 1,
-                  thickness: 0.2,
-                  indent: 10,
-                  endIndent: 10,
-                  color: Colors.black,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Text(
-                      '評価',
-                    style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-                  ),
-                ),
 
-                Padding(
-                  padding: EdgeInsets.only(top: 10, right: 30, bottom: 10, left: 30),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(top: 10, right:  70, bottom: 10, left: 0),
-                        child: Column(
-                          children: [
-                            Icon(
-                              Icons.sentiment_very_satisfied,
-                              color: Colors.grey,
-                              size: 70,
-                            ),
-                            Text(
-                                '良い'
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 10, right: 70, bottom: 10, left: 0),
-                        child: Column(
-                          children: [
-                            Icon(
-                              Icons.sentiment_neutral,
-                              color: Colors.grey,
-                              size: 70,
-                            ),
-                            Text(
-                                '普通'
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 10, right: 0, bottom: 10, left: 0),
-                        child: Column(
-                          children: [
-                            Icon(
-                              Icons.sentiment_very_dissatisfied,
-                              color: Colors.grey,
-                              size: 70,
-                            ),
-                            Text(
-                                '悪い'
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Divider(
-                  height: 1,
-                  thickness: 0.2,
-                  indent: 10,
-                  endIndent: 10,
-                  color: Colors.black,
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Text(
-                        '施設名',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: SizedBox(
-                        width: 330,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: 'サウナの施設名を入力',
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, right: 30, bottom: 10, left: 30),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 10, right:  70, bottom: 10, left: 0),
+                          child: Column(
+                            children: [
+                              Icon(
+                                Icons.sentiment_very_satisfied,
+                                color: Colors.grey,
+                                size: 70,
+                              ),
+                              Text(
+                                  '良い'
+                              ),
+                            ],
                           ),
-                          controller: _userNameController,
                         ),
-                      ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10, right: 70, bottom: 10, left: 0),
+                          child: Column(
+                            children: [
+                              Icon(
+                                Icons.sentiment_neutral,
+                                color: Colors.grey,
+                                size: 70,
+                              ),
+                              Text(
+                                  '普通'
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10, right: 0, bottom: 10, left: 0),
+                          child: Column(
+                            children: [
+                              Icon(
+                                Icons.sentiment_very_dissatisfied,
+                                color: Colors.grey,
+                                size: 70,
+                              ),
+                              Text(
+                                  '悪い'
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    Text(
-                        '選択',
-                      style: TextStyle(
-                          fontSize: 20,fontWeight: FontWeight.bold,
-                        color: Colors.blue,
-                      ),)
-                  ],
-                ),
-                Row(
-                  children: [
-                    Checkbox(
-                      checkColor: Colors.white,
-                      activeColor: Colors.blue,
-                      value: _flag,
-                      onChanged: _handleCheckbox,// チェックボックスをタップした際のイベントハンドラ
-                    ),
-                    Text('お気に入りに追加'),
-                  ],
-                ),
-                Divider(
-                  height: 1,
-                  thickness: 0.2,
-                  indent: 10,
-                  endIndent: 10,
-                  color: Colors.black,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Text(
-                    'メモ',
-                    style: TextStyle(
-                      fontSize: 20,fontWeight: FontWeight.bold,
-                    ),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextFormField(
-                    maxLines: 5,
-                    keyboardType: TextInputType.multiline,
-                    decoration: InputDecoration(border: OutlineInputBorder()),
                   ),
-                ),
-                Divider(
-                  height: 1,
-                  thickness: 0.2,
-                  indent: 10,
-                  endIndent: 10,
-                  color: Colors.black,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Text(
-                    '写真',
-                    style: TextStyle(
-                      fontSize: 20,fontWeight: FontWeight.bold,
-                    ),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
+                  Divider(
+                    height: 1,
+                    thickness: 0.2,
+                    indent: 10,
+                    endIndent: 10,
+                    color: Colors.black,
+                  ),
+                  Row(
                     children: [
-                      Icon(
-                        Icons.collections,
-                        size: 30,
-                        color: Colors.blue,
-                      ),
                       Padding(
-                        padding: const EdgeInsets.all(5.0),
+                        padding: const EdgeInsets.only(left: 10),
                         child: Text(
-                          '写真を選択',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.blue,
-                          ),),
+                          '施設名',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ],
                   ),
-                ),
-                Divider(
-                  height: 1,
-                  thickness: 0.2,
-                  indent: 10,
-                  endIndent: 10,
-                  color: Colors.black,
-                ),
-                Center(
-                  child: OutlinedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(
-                          context,
-                          SaunaPage.TAB.screenName,
-                        );
-                      },
-                      child: const Text('記録する')
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: SizedBox(
+                          width: 330,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'サウナの施設名を入力',
+                            ),
+                            controller: _userNameController,
+                          ),
+                        ),
+                      ),
+                      Text(
+                          '選択',
+                        style: TextStyle(
+                            fontSize: 20,fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                        ),)
+                    ],
                   ),
-                ),
-              ],
-            ),
+                  Row(
+                    children: [
+                      Checkbox(
+                        checkColor: Colors.white,
+                        activeColor: Colors.blue,
+                        value: _flag,
+                        onChanged: _handleCheckbox,// チェックボックスをタップした際のイベントハンドラ
+                      ),
+                      Text('お気に入りに追加'),
+                    ],
+                  ),
+                  Divider(
+                    height: 1,
+                    thickness: 0.2,
+                    indent: 10,
+                    endIndent: 10,
+                    color: Colors.black,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text(
+                      'メモ',
+                      style: TextStyle(
+                        fontSize: 20,fontWeight: FontWeight.bold,
+                      ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      maxLines: 5,
+                      keyboardType: TextInputType.multiline,
+                      decoration: InputDecoration(border: OutlineInputBorder()),
+                    ),
+                  ),
+                  Divider(
+                    height: 1,
+                    thickness: 0.2,
+                    indent: 10,
+                    endIndent: 10,
+                    color: Colors.black,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text(
+                      '写真',
+                      style: TextStyle(
+                        fontSize: 20,fontWeight: FontWeight.bold,
+                      ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.collections,
+                          size: 30,
+                          color: Colors.blue,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            '写真を選択',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.blue,
+                            ),),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    height: 1,
+                    thickness: 0.2,
+                    indent: 10,
+                    endIndent: 10,
+                    color: Colors.black,
+                  ),
+                  Center(
+                    child: OutlinedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(
+                            context,
+                            SaunaPage.TAB.screenName,
+                          );
+                        },
+                        child: const Text('記録する')
+                    ),
+                  ),
+                ],
+              ),
+          ),
       ),
     );
   }
